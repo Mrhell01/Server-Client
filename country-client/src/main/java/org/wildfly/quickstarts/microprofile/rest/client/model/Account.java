0f6e4485a3220ba48ab2a3bab14e8c1097940b83
Karan Panchal <panchalkaran967@gmail.com>
2023-02-24 01:00:33 +0530

    Client Server Working

---
+++ country-client/src/main/java/org/wildfly/quickstarts/microprofile/rest/client/model/Account.java
@@ -0,0 +1,25 @@
+package org.wildfly.quickstarts.microprofile.rest.client.model;
+
+public class Account {
+
+
+    public String name;
+    public int balance;
+    public int id;
+
+    public Account(){
+
+    }
+    public Account(String name , int balance,int id) {
+
+        this.name = name;
+        this.balance = balance;
+        this.id = id;
+
+    }
+
+
+
+}
+
+
